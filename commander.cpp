0845d101547e48498f0f956ec6a93fc27028a867
kcmetercec <kcmeter.cec@gmail.com>
2020-01-16 23:56:07 +0800

    feature(framework) : add command framework

--- commander.cpp
+++ commander.cpp
@@ -12,6 +12,8 @@ Commander::Commander(QObject *parent) : QObject(parent)
     connect(socket, SIGNAL(connected()), this, SLOT(connectSlot()));
     connect(socket, SIGNAL(error(QAbstractSocket::SocketError)),
             this, SLOT(errorSlot(QAbstractSocket::SocketError)));
+
+    head.ck = HEAD_CK;
 }
 void    Commander::connect2Server(const QString &ip, quint16 port)
 {
@@ -19,6 +21,17 @@ void    Commander::connect2Server(const QString &ip, quint16 port)
 
     socket->connectToHost(ip, port);
 
+}
+void    Commander::disconnect2Server(void)
+{
+    qDebug() << "disconnecting to server.";
+
+    if(hasConnected)
+    {
+        socket->disconnectFromHost();
+    }
+
+    hasConnected = false;
 }
 void    Commander::connectSlot(void)
 {
@@ -41,4 +54,39 @@ void    Commander::errorSlot(QAbstractSocket::SocketError err)
 void    Commander::recvSlot(void)
 {
 
+}
+bool    Commander::isConnect() const
+{
+    return hasConnected;
+}
+
+void    Commander::requestSysInfo(void)
+{
+    head.cmd = CLASS_INFO;
+    head.payload_len = 0;
+    send2Server();
+}
+bool    Commander::send2Server(void)
+{
+    if(!hasConnected)
+    {
+        qCritical() << "need connect to server first!";
+        return false;
+    }
+
+    if(socket->write((const char *)&head, sizeof(Header)) == -1)
+    {
+        qCritical() << "can't write data to server!";
+        return false;
+    }
+    if(head.payload_len)
+    {
+        if(socket->write(socketBuf, head.payload_len) == -1)
+        {
+            qCritical() << "can't write data to server!";
+            return false;
+        }
+    }
+
+    return true;
 }
